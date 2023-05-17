2590f6bb14c6d2694accdefad1443567f191fe4e
Simran <SBenipal@skillstorm.com>
2023-05-17 16:20:22 -0400

    Generated maven files and created a helloWorld Controller

---
+++ proj2-spring/src/main/java/com/skillstorm/demo/Proj2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj2SpringApplication.class, args);
+	}
+
+}
