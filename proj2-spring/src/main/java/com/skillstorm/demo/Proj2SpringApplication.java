851ca146092c89fecb26c26994f939ffe3b68a92
SimranBenipal1 <132481113+SimranBenipal1@users.noreply.github.com>
2023-05-18 15:28:29 -0400

    Update Proj2SpringApplication.java

--- proj2-spring/src/main/java/com/skillstorm/demo/Proj2SpringApplication.java
+++ proj2-spring/src/main/java/com/skillstorm/demo/Proj2SpringApplication.java
@@ -8,6 +8,7 @@ public class Proj2SpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proj2SpringApplication.class, args);
+		System.out.println("Server Started");
 	}
 
 }
