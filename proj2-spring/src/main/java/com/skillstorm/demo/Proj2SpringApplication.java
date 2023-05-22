ad376dadd59cad7add5a55ec15fee540c2518d1f
Simran <SBenipal@skillstorm.com>
2023-05-22 17:46:38 -0400

    Added dev and prod env's

--- proj2-spring/src/main/java/com/skillstorm/demo/Proj2SpringApplication.java
+++ proj2-spring/src/main/java/com/skillstorm/demo/Proj2SpringApplication.java
@@ -1,13 +1,25 @@
 package com.skillstorm.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Proj2SpringApplication {
+public class Proj2SpringApplication implements CommandLineRunner{
 
+	@Value("${my.env.mode}") 
+	private String mode;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Proj2SpringApplication.class, args);
+		System.out.println("Spring server has started");
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Currently in: " + mode);
 	}
 
 }
