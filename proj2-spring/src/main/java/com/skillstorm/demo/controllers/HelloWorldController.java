9b7776ee74d358b4366616a302633e9b53b93668
Simran <SBenipal@skillstorm.com>
2023-05-24 16:37:57 -0400

    Added the current mode to the hello controller

--- proj2-spring/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
+++ proj2-spring/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
@@ -1,13 +1,17 @@
 package com.skillstorm.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Value("${my.env.mode}") 
+	private String mode;
+	
 	@GetMapping("/hello")
 	public String helloWorld() {
-		return "Hello World! The CICD Pipeline Works!";
+		return "Hello World! The CICD Pipeline Works! You are Currently in: " + mode;
 	}
 }
